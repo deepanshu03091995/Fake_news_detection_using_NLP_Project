425d0a55a6fd287052e580708865f64b88885153
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2024-02-20 14:30:53 +0530

    s3_syncer renamed to s3_operations

--- template.py
+++ template.py
@@ -15,7 +15,7 @@ list_of_files = [
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
     f"{project_name}/configuration/__init__.py",
-    f"{project_name}/configuration/S3_syncer.py",
+    f"{project_name}/configuration/s3_operations.py",
     f"{project_name}/constants/__init__.py",
     f"{project_name}/entity/__init__.py",
     f"{project_name}/entity/config_entity.py",
